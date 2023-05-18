afaf998f7c61d1f2aac53d3a50205905f4d35104
PeeterMelo <115720708+PeeterMelo@users.noreply.github.com>
2023-05-18 10:53:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+
+const menu = document.querySelector('.menu');
+
+menuHamburguer.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
